84673b0a22089ebcbd83a4da36ae17b122faa6df
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-01-11 17:14:32 +0530

    Apply base url

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
 // export const base_url = "http://localhost:5000/api/v1/";
-export const base_url = "https://train-reservation-vqyw.onrender.com";
+export const base_url = "https://train-reservation-vqyw.onrender.com/api/v1/";
